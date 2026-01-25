ac23c973c7f4a36743d2da233bac351288379c3e
Rebeca Murcia <131334317+RebecaMurcia@users.noreply.github.com>
2026-01-24 21:22:33 -0800

    PUT, POST, DELETE requests - Week 03

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -10,16 +10,93 @@ const getAll = async (req, res, next) => {
 };
 
 const getSingle = async (req, res, next) => {
+  try {
+    const userId = new ObjectId(req.params.id);
+    const result = await mongodb
+      .getDb()
+      .db('baseDB')
+      .collection('contacts')
+      .findOne({ _id: userId }); // Changed find() to findOne()
+
+    if (result) {
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json(result);
+    } else {
+      res.status(404).json({ message: 'Contact not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const createContact = async(req,res) => {
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+
+  const response = await mongodb
+  .getDb()
+  .db()
+  .collection('contacts')
+  .insertOne(contact);
+
+  if (response.acknowledged) {
+    res.status(201).json(response);
+  } else {
+    res.status(500).json(response.error || 'Some error occured while creating the contact.');
+  }
+};
+
+const updateContact = async (req, res) => {
+  if (!ObjectId.isValid(req.params.id)) {
+    res.status(400).json('Must use a valid contact id to update a contact.');
+  }
   const userId = new ObjectId(req.params.id);
-  const result = await mongodb
+  
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+
+  const response = await mongodb
     .getDb()
     .db()
     .collection('contacts')
-    .find({ _id: userId });
-  result.toArray().then((lists) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(lists[0]);
-  });
+    .replaceOne({ _id: userId }, contact);
+
+  console.log(response);
+  if (response.modifiedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while updating the contact.');
+  }
 };
 
-module.exports = { getAll, getSingle };
+const deleteContact = async (req, res) => {
+  if (!ObjectId.isValid(req.params.id)) {
+    res.status(400).json('Must use a valid contact id to delete a contact.');
+  }
+
+  const userId = new ObjectId(req.params.id);
+const response = await mongodb
+    .getDb()
+    .db()
+    .collection('contacts')
+    .deleteOne({ _id: userId }, true);
+
+  console.log(response);
+  if (response.deletedCount > 0) {
+    res.status(200).send();
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while deleting the contact.');
+  };
+};
+
+module.exports = { getAll, getSingle,createContact,updateContact, deleteContact};
