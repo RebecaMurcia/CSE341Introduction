6523769391bb315fe8b5d51b3871ec96e4b53926
Rebeca Murcia <131334317+RebecaMurcia@users.noreply.github.com>
2026-01-31 21:10:22 -0800

    API documentation week04

--- server.js
+++ server.js
@@ -3,13 +3,19 @@ const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
 
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080;;
 const app = express();
 
 app
   .use(bodyParser.json())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Acces-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Z-Key');
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+
     next();
   })
   .use('/', require('./routes'));
